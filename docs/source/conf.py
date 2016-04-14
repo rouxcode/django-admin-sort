e459fb586167ae0e3ac6f055a2a99128fd2caed9
benzkji <bnzk@bnzk.ch>
2016-04-14 09:41:23 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'adminsortable2doc'
+htmlhelp_basename = 'adminsortdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
