7e171f0f1f9ade40b7d4fd115baece0e361a7792
benzkji <bnzk@bnzk.ch>
2018-10-29 15:28:59 +0100

    MIDDLEWARE for django 2+

--- admin_sort/tests/settings.py
+++ admin_sort/tests/settings.py
@@ -79,7 +79,7 @@ INSTALLED_APPS = [
     'admin_sort.tests.testapp',
 ]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
