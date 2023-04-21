215c8077bddebb60a2722ee6bd0ca34c38a7ab0e
Ben Stähli <bnzk@bnzk.ch>
2023-04-21 15:54:21 +0200

    remove print

--- manage.py
+++ manage.py
@@ -14,6 +14,5 @@ if __name__ == "__main__":
     try:
         execute_from_command_line(sys.argv)
     except Exception as exception:
-        print(exception.__repr__())
         traceback.print_exc()
         raise exception
